3873a963d9fbb28b6af5e0fec44b510bc81f6900
Alexander Walz <alexwalz@icloud.com>
2018-07-01 22:25:11 -0600

    api routes

--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -8,40 +8,6 @@ const db_url = process.env.MONGODB_URI || config.dbUri
 
 mongoose.connect(db_url);
 
-// router.get('/', function (req, res) {
-//     res.json({ message: 'API Initialized!' });
-// });
-
-// router.get('/test', function (req, res) {
-//     res.status(200).json({
-//         message: "You're authorized to see this secret message."
-//     });
-// });
-
-// USER ROUTES
-// router.route('/users')
-//     .get(function (req, res) {
-//         User.find(function (err, users) {
-//             if (err)
-//                 res.send(err);
-//             res.json(users)
-//         });
-//     })
-//     .post(function (req, res) {
-//         const user = new User();
-
-//         user.first_name = req.body.first_name;
-//         user.last_name = req.body.last_name;
-//         user.email = req.body.email;
-//         user.password = req.body.password;
-//         user.role = req.body.role;
-//         user.save(function (err) {
-//             if (err)
-//                 res.send(err);
-//             res.json({ message: 'User successfully created' });
-//         });
-//     });
-
 router.route('/send-message')
     .post(function (req, res) {
         // Generate test SMTP service account from ethereal.email
