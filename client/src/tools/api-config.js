5db31f16c03b6a6eb95816e5c9220e4cef3f2609
Alexander Walz <alexwalz@icloud.com>
2018-07-01 22:32:33 -0600

    .

--- client/src/tools/api-config.js
+++ client/src/tools/api-config.js
@@ -7,7 +7,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === 'localhost') {
   backendHost = 'http://localhost:5000';
 } else {
-  backendHost = 'https://boinblebox.herokuapp.com';
+  backendHost = 'https://boinglebox.herokuapp.com';
 }
 
 export const API_URL = `${backendHost}`;
