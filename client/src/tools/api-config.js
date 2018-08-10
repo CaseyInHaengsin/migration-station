d714f52146031dd7cf2118e07577f80f44b0637e
Alexander Walz <alexwalz@icloud.com>
2018-08-09 22:24:15 -0600

    .

--- client/src/tools/api-config.js
+++ client/src/tools/api-config.js
@@ -7,7 +7,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === 'localhost') {
   backendHost = 'http://localhost:5000';
 } else {
-  backendHost = 'https://boinglebox.herokuapp.com';
+  backendHost = 'https://marina-cove-storage.herokuapp.com';
 }
 
 export const API_URL = `${backendHost}`;
